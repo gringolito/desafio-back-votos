24529621d516b2575f5015d65c14239e4c8c1521
Filipe Utzig <filipeutzig@gmail.com>
2020-05-23 11:04:06 -0300

    Initial Spring Boot Initializr application
    This initializr project includes the following depedencies:
    - Spring Web;
    - Spring Data-JPA;
      - Hibernate;
      - PostgreSQL;
    - Spring Actuator;
    - Lombok.
    
    Signed-off-by: Filipe Utzig <filipeutzig@gmail.com>

---
+++ src/main/java/com/southsystem/cooperativeassembly/CooperativeAssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.southsystem.cooperativeassembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeAssemblyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooperativeAssemblyApplication.class, args);
+    }
+
+}
